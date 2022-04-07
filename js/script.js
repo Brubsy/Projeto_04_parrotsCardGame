394c765f83028b5e5e436e4c9c425ac526060b83
Bruna Vieira <brunavieira@nano.ufrj.br>
2022-04-07 11:42:50 -0300

    Restrição da quantidade de cartas e de quantidades ímpares

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@ function setUpGame() {
     const cardsQuantity = prompt("Com quantas cartas você quer jogar? (mín. 4 e máx. 14)");
     const addCard = document.querySelector(".cards");
 
-    for (let i = 0; i < cardsQuantity; i++) {
-        addCard.innerHTML += `
-        <li class="card">
-            <img src="./images/front.png">
-        </li>`
+    if (4 <= cardsQuantity && cardsQuantity<= 14 && cardsQuantity % 2 === 0) {
+        for (let i = 0; i < cardsQuantity; i++) {
+            addCard.innerHTML += `
+            <li class="card">
+                <img src="./images/front.png">
+            </li>`
+        }
+    } else {
+        alert("Quantidade inválida!")
+        setUpGame();   
     }
 }
 
