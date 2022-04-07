d52691f853d378d593bb17be101b36932728ae16
Bruna Vieira <brunavieira@nano.ufrj.br>
2022-04-07 11:22:55 -0300

    Ao entrar no jogo, o usuário deverá ser perguntado com quantas cartas quer jogar

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-//function setUpGame() {
-    //const cardsQuantity = prompt("Com quantas cartas você quer jogar? (mín. 4 e máx. 14)");
+function setUpGame() {
+    const cardsQuantity = prompt("Com quantas cartas você quer jogar? (mín. 4 e máx. 14)");
+    const addCard = document.querySelector(".cards");
 
-    //for (let i = 0; i < cardsQuantity; i++) {
+    for (let i = 0; i < cardsQuantity; i++) {
+        addCard.innerHTML += `
+        <li class="card">
+            <img src="./images/front.png">
+        </li>`
+    }
+}
 
-    //}
-//}
-
-
-//setUpGame();
+setUpGame();
