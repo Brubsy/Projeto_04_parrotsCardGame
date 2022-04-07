243cfca6f146974dee870b0958523f15e70caf7d
Bruna Vieira <brunavieira@nano.ufrj.br>
2022-04-07 11:15:57 -0300

    Troca das divs das cartas por ul e li

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//function setUpGame() {
+    //const cardsQuantity = prompt("Com quantas cartas você quer jogar? (mín. 4 e máx. 14)");
+
+    //for (let i = 0; i < cardsQuantity; i++) {
+
+    //}
+//}
+
+
+//setUpGame();
